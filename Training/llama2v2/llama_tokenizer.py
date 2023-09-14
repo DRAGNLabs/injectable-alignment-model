19c66acc72afd28c7bd2b4d8c2366d9f55c0f377
Drew Galbraith <dsg2060@login01.rc.byu.edu>
2023-09-14 17:42:44 -0600

    fixing the problems

--- Training/llama2v2/llama_tokenizer.py
+++ Training/llama2v2/llama_tokenizer.py
@@ -21,14 +21,14 @@ class Tokenizer:
         self.n_words: int = self.sp_model.vocab_size()
         self.bos_id: int = self.sp_model.bos_id()
         self.eos_id: int = self.sp_model.eos_id()
-        self.pad_id: int = self.sp_model.pad_id()
+        self.pad_id: int = self.sp_model['<pad>']
         logger.info(
-            f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}"
+            f"# of words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}"
         )
         # print(f"\n Pad Token ID: {self.pad_id}\n",f"BOS Token ID: {self.bos_id}\n", f"EOS Token ID: {self.eos_id}\n")
         assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
 
-    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+    def encode(self, s: str, bos: bool, eos: bool, seq_len: int) -> List[int]:
         assert type(s) is str
         t = self.sp_model.encode(s)
         if bos:
@@ -36,27 +36,39 @@ class Tokenizer:
         if eos:
             t = t + [self.eos_id]
         return t
+        
+            
+
 
     def decode(self, t: List[int]) -> str:
         return self.sp_model.decode(t)
     
 
 
-def tokenize_data_chunk(tokenizer, chunk):  
+def tokenize_data_chunk(self, tokenizer, chunk, seq_len):  
     '''
     Take some tokenizer object and some dictionary-like(?) data format
     TODO: look into padding and sequence length for our tokenizer
     ''' 
     # print(chunk)
     to_tokenize:str = chunk['system_prompt'] + '<SEP>' + chunk['question'] + '<SEP>' + chunk['response']
-    chunk['Tokenized_Data'] = tokenizer.encode(to_tokenize, bos=True, eos=True)
+    chunk['Tokenized_Data'] = tokenizer.encode(to_tokenize, bos=True, eos=True, seq_len=seq_len)
+    
+    # Add padding
+    if len(chunk) >= seq_len:
+        chunk = chunk[:seq_len]
+    else:
+        deficient:int = seq_len - len(chunk)
+        pads = [self.pad_id]*deficient
+        chunk = chunk + pads
+
     # print(chunk.columns)
     return chunk
 
-def generate_tokenized_file(df:pd.DataFrame, path_to_model):
+def generate_tokenized_file(df:pd.DataFrame, path_to_model, seq_len=1000):
     # Call 'tokenize_data_chunk' over entire file
     tokenizer = Tokenizer(path_to_model)
-    tok_lambda = lambda x: tokenize_data_chunk(tokenizer, x)  # 'df.' of line 69 becomes 'x' in this lambda
+    tok_lambda = lambda x: tokenize_data_chunk(tokenizer, x, seq_len=seq_len)  # 'df.' of line 62 becomes 'x' in this lambda
     print("\033[0;33m")  # Turn text orange
     df1 = df.progress_apply(tok_lambda, axis=1)
     print("\033[0;37m")  # return text to white
@@ -85,7 +97,7 @@ def main():
     training_dataframe:pd.DataFrame = load_datasets(data_files).iloc[2000000:]
 
     # Generate tokenized file
-    tokenized_df:pd.DataFrame = generate_tokenized_file(training_dataframe, path_to_model=model_path)
+    tokenized_df:pd.DataFrame = generate_tokenized_file(training_dataframe, path_to_model=model_path, seq_len=1000)
     out_dir = "./tokenized_files/"
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
