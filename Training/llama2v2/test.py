1f08c598c4d3740e5ac849ed63baf2e2b3249445
Jackson Searle <jacksonsearle24@gmail.com>
2023-09-14 18:39:23 -0600

    stuff

--- Training/llama2v2/test.py
+++ Training/llama2v2/test.py
@@ -1,6 +1,20 @@
-import torch
+import pickle
 
-# example with padding_idx
-embedding = torch.nn.Embedding(10, 3, padding_idx=-1)
-input = torch.LongTensor([[0, 2, 0, 5]])
-print(embedding(input))
+# Open the pickle file for reading
+with open('../dataset/tokenized_files/toy_tokenized_data.pkl', 'rb') as file:
+    # Load the data from the pickle file
+    df = pickle.load(file)
+
+# Now you can use the loaded_data in your code
+print(df)
+print(type(df))
+
+print(df.iloc[1][1])
+# Function to replace -1 with 32000
+def replace_minus_one(val):
+    return [32000 if x == -1 else x for x in val]
+
+# Apply the function to the Tokenized_Data column
+df['Tokenized_Data'] = df['Tokenized_Data'].apply(replace_minus_one)
+
+print(df['Tokenized_Data'].iloc[2])
