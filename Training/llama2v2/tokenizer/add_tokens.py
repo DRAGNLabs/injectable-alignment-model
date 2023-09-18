e75aa07226d9785a940c40ca21a62695466211d0
Drew Galbraith <dsg2060@login03.rc.byu.edu>
2023-09-18 13:16:12 -0600

    Make changes to code

--- Training/llama2v2/tokenizer/add_tokens.py
+++ Training/llama2v2/tokenizer/add_tokens.py
@@ -1,30 +1,63 @@
 import sentencepiece as sp
 from sentencepiece import sentencepiece_model_pb2 as model
 
-sp_old = sp.SentencePieceProcessor(model_file='../../tokenizer.model')
-
+file_path = "./toy_tokenizer.model"
+sp_old = sp.SentencePieceProcessor(model_file=file_path)
+print('\n\n\n  1  \n\n\n')
 # Add tokens to tokenizer dict
-m = model.ModelProto()
-m.ParseFromString(open("test_tokenizer.model", "rb").read())
 
-special_tokens: List[str] = ['<pad>']  #Add tokens here; e.g. '<pad>', '<UNK>', etc.
+def add_tokens(file_path, new_tokens):
+    # Load tokenizer model
+    m = model.ModelProto()
+    m.ParseFromString(open(file=file_path, mode="rb").read())
+    print('\nViewing tokens...\n')
+    # Add tokens
+    for token in new_tokens:
+        new_token = model.ModelProto().SentencePiece()
+        new_token.piece = token
+        new_token.score = 0
+        m.pieces.append(new_token)
 
-for token in special_tokens:
-    new_token = model.ModelProto().SentencePiece()
-    new_token.piece = token
-    new_token.score = 0
-    m.pieces.append(new_token)
+    # Write new model out to the same name
+    with open(file_path, 'wb') as outf:
+        outf.write(m.SerializeToString())
+    print('written out')
 
-with open('../../tokenizer.model', 'wb') as outf:
-    outf.write(m.SerializeToString())
-
-sp_new = sp.SentencePieceProcessor(model_file='../../tokenizer.model')
-
-# Ensure successful addition of special tokens
-if sp_old.get_piece_size() + len(special_tokens) == sp_new.get_piece_size():
-    for token in special_tokens:
+def check_submission(tokens_to_check, path_to_file):
+    sp_new = sp.SentencePieceProcessor(model_file=path_to_file)
+    sp
+    # Ensure successful addition of special tokens
+    if sp_old.get_piece_size() + len(tokens_to_check) == sp_new.get_piece_size():
+        for token in tokens_to_check:
+            print(sp_new[token])
+        print(f'The old vocab length was {sp_old.get_piece_size()} tokens\n \
+        After adding {len(special_tokens)} more tokens, the new vocab length is: {sp_new.get_piece_size()} tokens.')
+    else:
+        print('!!!!!!!!!!!!!!!!\nError: The new tokens were not added correctly!\n!!!!!!!!!!!!!!!!')
+    for token in tokens_to_check:
         print(sp_new[token])
     print(f'The old vocab length was {sp_old.get_piece_size()} tokens\n \
-      After adding {len(special_tokens)} more tokens, the new vocab length is: {sp_new.get_piece_size()} tokens.')
-else:
-    print('!!!!!!!!!!!!!!!!\nError: The new tokens were not added correctly!\n!!!!!!!!!!!!!!!!')
+    After adding {len(special_tokens)} more tokens, the new vocab length is: {sp_new.get_piece_size()} tokens.')
+
+def export_vocab(file_path):
+
+    # Create an instance of the SentencePieceProcessor class
+    spp = sp.SentencePieceProcessor()
+
+    # Load the SentencePiece model
+    spp.Load(file_path)
+
+    # Get the number of pieces in the vocabulary
+    piece_size = spp.GetPieceSize()
+
+    # Iterate through the pieces and write them to a file
+    with open(f'vocab.txt', 'w') as outf:
+        for i in range(piece_size):
+            piece = spp.IdToPiece(i)
+            outf.write(str(i) + '\t' + piece + '\n')
+
+special_tokens: list[str] = ['bananaaa']  # Add tokens here; e.g. '<pad>', '<UNK>', etc.
+
+# add_tokens(file_path=file_path, new_tokens=special_tokens)
+# check_submission(special_tokens, path_to_file=file_path)
+# export_vocab(file_path=file_path)
