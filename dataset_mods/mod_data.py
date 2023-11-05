9e4aa7a8a760dc2db9420d4e894fc4950e433025
DrewGalbraith <drew.s.galbraith@gmail.com>
2023-11-04 21:56:22 -0600

    New dataset prep branch; add all sorts of stuff

--- dataset_mods/mod_data.py
+++ dataset_mods/mod_data.py
@@ -12,10 +12,24 @@
             with a simplified vocab. Afterward, we can check to see how well the TinyStories
             dataset follows it's own claim, assuming your pipeline runs well. But this
             is a key aspect of combining the two methodologies."""
+## Pseudocode
+# 1. Load vocab csv into dict and load prompts into pandas df
+# 2. for prompt in prompts
+#   3. tokenize prompt
+#   4. for word in prompt:
+#       5. if word not in prompt:
+#           6. Append word to trouble_list
+#       7. if len(trouble_list)!=0:
+#           8. append to flagged_list
+#       9. else:
+#           10. append to clear_list
+#       11. if len(clear_list)==N or len(flagged_list)==N: # write out progress every N to 2N prompts
+#           12. Write out to .csvs
 
 import csv, pickle
 import pandas as pd
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize as w_t
 
 
 def convert_xlsx_to_csv(input_xlsx_file, output_csv_file):
@@ -32,22 +46,7 @@ def convert_xlsx_to_csv(input_xlsx_file, output_csv_file):
 # convert_xlsx_to_csv(input_file_path, output_file_path)
 
 
-## Pseudocode
-# 1. Load vocab csv into dict and load prompts into pandas df
-# 2. for prompt in prompts
-#   3. tokenize prompt
-#   4. for word in prompt:
-#       5. if word not in prompt:
-#           6. Append word to trouble_list
-#       7. if len(trouble_list)!=0:
-#           8. append to flagged_list
-#       9. else:
-#           10. append to clear_list
-#       11. if len(clear_list)==N or len(flagged_list)==N: # write out progress every N to 2N prompts
-#           12. Write out to .csvs
-
 # 1.
-
 def load_csv_to_dict(file_path, pickle_dict=False):
     data_dict = {}
     with open(file_path, newline='') as csvfile:
@@ -91,18 +90,106 @@ result_dict = load_pickle_to_dict(file_path)
 
 # print(len(result_dict)) # 1,041 unique word forms 
 
+def stop_word_stats(stop_words):
+    ## Caluculate the amount of stop words not included in the 44k words;
+    ## 97 of 179 are not included (~54%), and many of them are contractions.
+    
+    # not_included = []
+    # for word in stop_words:
+    #     try:
+    #         result_dict[word]
+    #     except KeyError:
+    #         not_included.append(word)
+    # print(len(not_included), not_included)
+
 # Get English stopwords
-stop_words = stopwords.words('english')
+# stop_words = stopwords.words('english')
+# stop_word_stats(stop_words=stop_words)
+
+def parquet_to_dict(file_path):
+    try:
+        # Read the Parquet file using pandas
+        data = pd.read_parquet(file_path)
+
+        # Assuming the first column is used as keys and the rest as values
+        keys = data.iloc[:, 0]  # Extracting the first column as keys
+        values = data.iloc[:, 1:]  # Extracting the rest as values
+
+        # Convert the DataFrame to a dictionary
+        result_dict = {}
+        for i, key in enumerate(keys):
+            result_dict[key] = values.iloc[i, :].to_list()
+
+        return result_dict
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None  # Return None if an error occurs
+
+file_path = "some_parquet_file.parquet"
+open_orca_dataset = parquet_to_dict(file_path)
+if open_orca_dataset is not None:
+    print("\nWriting to dict successfu!\nl")
+
+nltk.download('punkt')  # Download the 'punkt' tokenizer models (if not already downloaded)
+
+def tokenize_with_penn_treebank(text):
+    # Tokenize the input text using the Penn Treebank Word Tokenizer
+    tokens = w_t(text, language='english', preserve_line=False)  # nltk.word_tokenize
+    return tokens
+
+def eval_prompt(words_list, vocab_dict):
+    # Search for each word in the list and create tuples based on the dictionary's presence
+    output_data = []
+    flagged = False
+    for word in words_list:
+        if word in vocab_dict:
+            output_data.append((word, 0))
+        else:
+            output_data.append((word, 1))
+            flagged = True
+    return (flagged, output_data)
+
+def write_to_csv(output_file, output_data):
+    # Write the tuples to a CSV file
+    with open(output_file, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerows(output_data)
+
+def flag_prompts(prompts_dict, vocab_dict, output_file, write_every=1000):
+    flagged_data = []
+    clean_data = []
+
+    for prompt in prompts_dict:
+        #todo: Add comments; make this keep prompts and questions together; figure out how to best write out list items to a csv
+        tokenized_prompt = tokenize_with_penn_treebank(prompt)
+        prompt_analyzed = eval_prompt(tokenized_prompt)
+        if prompt_analyzed[0]:
+            flagged_data.append(prompt_analyzed)
+        else:
+            clean_data.append(prompt_analyzed)
+
+        if len(flagged_data) % write_every == 0:
+            write_to_csv(output_file, flagged_data)
+            flagged_data = []
+
+        if len(clean_data) % write_every == 0:
+            write_to_csv(output_file, clean_data)
+            clean_data = []
+    
 
 
-## Caluculate the amount of stop words not included in the 44k words;
-## 97 of 179 are not included (~54%), and many of them are contractions.
 
-# not_included = []
-# for word in stop_words:
-#     try:
-#         result_dict[word]
-#     except KeyError:
-#         not_included.append(word)
-# print(len(not_included), not_included)
-
+## Pseudocode
+# 1. Load vocab csv into dict and load prompts into pandas df
+# 2. for prompt in prompts
+#   3. tokenize prompt
+#   4. for word in prompt:
+#       5. if word not in prompt:
+#           6. Append word to trouble_list
+#       7. if len(trouble_list)!=0:
+#           8. append to flagged_list
+#       9. else:
+#           10. append to clear_list
+#       11. if len(clear_list)==N or len(flagged_list)==N: # write out progress every N to 2N prompts
+#           12. Write out to .csvs
