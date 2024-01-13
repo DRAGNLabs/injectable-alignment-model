c9598167fea961b2979f4b4e5d06c3ff89b1a15d
Clayton Chase <ccchase7@login01.rc.byu.edu>
2024-01-13 12:23:13 -0700

    Add working files

--- slurm/convert_checkpoint.sh
+++ slurm/convert_checkpoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #SBATCH --time=01:00:00   # walltime
-#SBATCH --ntasks-per-node=2   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --gpus=2
-#SBATCH --mem-per-cpu=64GB   # memory per CPU core
-#SBATCH -J "single_train_IRM"   # job name
-
+#SBATCH --mem-per-cpu=32G   # memory per CPU core
+#SBATCH --gpus=1
+#SBATCH -J "tokenize_data"   # job name
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 mamba activate rocket
-srun python3 ../injected_train_single.py ../configs/winter_train_single_config.yaml
+
+srun python3 convert_checkpoint.py configs/convert_checkpoint_config.yaml /home/ccchase7/CS301R/IRM/injectable-alignment-model/injected_model_weights_6_7.ckpt
--- slurm/tokenize_data.sh
+++ slurm/tokenize_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=03:00:00   # walltime
+#SBATCH --time=01:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=32G   # memory per CPU core
@@ -12,4 +12,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 mamba activate rocket
 
-srun python3 ../tokenize_data.py ../configs/train_custom_tokenizer_large.yaml
+srun python3 ../tokenize_data.py ../configs/tokenize_emotions_config.yaml
