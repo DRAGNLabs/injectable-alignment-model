b2bfc3055b7dc86bb36b8cbe7b4e0ff01cc576a2
Clayton Chase <ccchase7@login03.rc.byu.edu>
2024-02-21 16:01:29 -0700

    Initial commit to restructure for using HF weights

--- slurm/setup.sh
+++
@@ -1,2 +0,0 @@
-# Download and prepare data, train tokenizer, and tokenize data
-python /grphome/grp_inject/injectable-alignment-model/setup.py && sbatch train_tokenizer.sh && sbatch tokenize_data.sh
