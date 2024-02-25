ef0f44af0f624f5bfc8d9f704dab2e837c6b8d0a
Dallin Baker <dfbaker5@login02.rc.byu.edu>
2024-02-24 17:49:51 -0700

    set up IRM in injected classes

--- Rocket-Launch/src/llama_models/irm.py
+++ Rocket-Launch/src/llama_models/irm.py
@@ -1,29 +1,40 @@
 import torch.nn as nn
 import torch
 
+from transformers import LlamaConfig
+
 class IRM(nn.Module):
-    def __init__(self, config, size_modifier = 1):
+    def __init__(self, config: LlamaConfig, size_modifier = 1):
         super(IRM, self).__init__()
-        self.weights: [torch.Tensor] = []
+        self.weights: torch.Tensor = []
+        # self.weights: [torch.Tensor] = []
         self.linear_size = 50 ##
         self.device = torch.device('cuda')
 
-        self.batch_size = config.batch_size
-        self.sequence_length = config.sequence_length
-        self.dim = config.dim
-        self.output_dimensions = (self.sequence_length, self.dim)
+        self.vocab_size = config.vocab_size
+        self.max_position_embeddings = config.max_position_embeddings
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = config.intermediate_size
+        self.num_hidden_layers = config.num_hidden_layers
+        self.num_attention_heads = config.num_attention_heads
+        
 
-        self.injection_layers = config.IRM_layers
+        # self.batch_size = config.batch_size
+        # self.sequence_length = config.sequence_length
+        # self.dim = config.dim
+        self.output_dimensions = (self.hidden_size, self.num_attention_heads)
+
+        self.injection_layers = [i for i in range(self.num_hidden_layers)] #[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
         self.num_layers = len(self.injection_layers)
         
         self.basic_forward = nn.Sequential(
-            nn.Linear(self.dim, self.linear_size*size_modifier),
+            nn.Linear(self.hidden_size, self.linear_size*size_modifier),
             nn.ReLU(),
             nn.Linear(self.linear_size*size_modifier, self.linear_size*size_modifier),
             nn.ReLU(),
-            # nn.Linear(self.linear_size*size_modifier, self.linear_size*size_modifier), #this is causing a problem
-            # nn.ReLU(),
-            nn.Linear(self.linear_size*size_modifier, self.dim * self.num_layers),
+            nn.Linear(self.linear_size*size_modifier, self.linear_size*size_modifier),
+            nn.ReLU(),
+            nn.Linear(self.linear_size*size_modifier, self.hidden_size, self.num_attention_heads),
         ).to(self.device)
 
     def forward(self, x: torch.Tensor):
@@ -36,10 +47,8 @@ class IRM(nn.Module):
     def injected_operation(self, layer_id, llm_output):
         return self.get_layer_weights(layer_id) + llm_output
 
-
-
 if __name__ == "__main__":
-    model = IRM()
-    model.forward(torch.randn((1,1024,512)))
+    model = IRM(LlamaConfig())
+    # model.forward(torch.randn((1,1024,512)))
     model.forward(torch.randn((1,1024,512)))
     print(model.weights[3])
