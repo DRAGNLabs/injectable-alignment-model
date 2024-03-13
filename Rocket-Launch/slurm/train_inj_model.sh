141e061fa1b34f9daa3c597325d893471d89fa62
Clayton Chase <ccchase7@login03.rc.byu.edu>
2024-03-13 12:05:05 -0600

    Add more gpus

--- Rocket-Launch/slurm/train_inj_model.sh
+++ Rocket-Launch/slurm/train_inj_model.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --time=00:10:00   # walltime
-#SBATCH --ntasks-per-node=1 # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --mem=128G   # memory per CPU core
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=4 # number of processor cores (i.e. tasks)
+#SBATCH --nodes=4   # number of nodes
+#SBATCH --mem=512G   # memory per CPU core
+#SBATCH --gres=gpu:4
 #SBATCH --qos=cs
 #SBATCH -J "train_model"   # job name
 
@@ -14,4 +14,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 nvidia-smi
 mamba activate rocket
-srun python3 ../src/injected_train.py ../configs/test_config_0_1_2_3.yaml
+srun python3 ../src/injected_train.py ../configs/test_config_anger_QA_13b_2.pkl_0_1_2_3.yaml
