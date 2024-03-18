6027534de657d222697153d74eb3248905f8bfc0
Clayton Chase <ccchase7@login03.rc.byu.edu>
2024-03-18 12:52:38 -0600

    Increase walltime

--- Rocket-Launch/slurm/train_inj_model.sh
+++ Rocket-Launch/slurm/train_inj_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=00:10:00   # walltime
+#SBATCH --time=03:00:00   # walltime
 #SBATCH --ntasks-per-node=2 # number of processor cores (i.e. tasks)
 #SBATCH --nodes=2   # number of nodes
 #SBATCH --mem=512G   # memory per CPU core
@@ -14,4 +14,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 nvidia-smi
 mamba activate rocket
-srun python3 ../src/injected_train.py ../configs/test_config_anger_QA_13b_2.pkl_0_1_2_3.yaml
+srun python3 ../src/injected_train.py ../configs/test_config_anger_QA_13b_2.pkl_1_3_5_7_9_11_13_15_17_19_21_23_25_27_29_31.yaml 
