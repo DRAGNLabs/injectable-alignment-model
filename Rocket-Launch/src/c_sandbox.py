7ff62bf86e4ec8fec8795bc4b443e7064fc6a1e6
Clayton Chase <ccchase7@login02.rc.byu.edu>
2024-03-22 14:12:13 -0600

    Add run with deactivated irm setting

--- Rocket-Launch/src/c_sandbox.py
+++ Rocket-Launch/src/c_sandbox.py
@@ -32,11 +32,15 @@ def generate_from_model(model_type, tokenizer, config, prompt_list=["Hey there! 
         checkpoint = torch.load(static_weights_path)
         model.load_state_dict(checkpoint['state_dict'])
     # Loading a model injected with an IRM (path specified in config file)
-    elif model_type == "irm_load":
+    elif model_type in ["irm_load", "irm_deactivated"]:
         model = IRM_Model(tokenizer, config)
 
         checkpoint = torch.load(config.checkpoint_path)
         model.load_state_dict(checkpoint['state_dict'])
+
+        # Deactivate any IRM contributions, so the IRM should behave as the base model
+        if model_type == "irm_deactivated":
+            model.model.irm.deactivate()
 
     model.eval()
     model.to(device)
@@ -47,7 +51,7 @@ def generate_from_model(model_type, tokenizer, config, prompt_list=["Hey there! 
         max_gen_len = 50
         temperature = None
         top_p = None
-        repetition_penalty = None
+        repetition_penalty = None        
 
         generate_ids = model.generate(prompt_tokens.to(device), 
                                         max_length=max_gen_len, 
@@ -64,7 +68,7 @@ def generate_from_model(model_type, tokenizer, config, prompt_list=["Hey there! 
 
 device = torch.device('cuda:0' if 'CUDA_VISIBLE_DEVICES' in os.environ else 'cpu')
 
-config_path = "../configs/test_config_anger_QA_13b_2.pkl_1_3_5_7_9_11_13_15_17_19_21_23_25_27_29_31.yaml"
+config_path = "../configs/config_Llama-2-7b-hf_anger_QA_13b_2.pkl_0_1_2_3_4_5_6_7_8_9_10_11_12_13_14_15_16_17_18_19_20_21_22_23_24_25_26_27_28_29_30_31.yaml"
 
 with open(config_path, "r") as f:
     config = yaml.safe_load(f)
@@ -84,8 +88,8 @@ else:
     raise ValueError(f"Tokenizer type '{config.tokenizer_type}' not recognized. Must be 'hf' or 'sp'.")
 
 
-model_types = ["hf_load", "static_load", "irm_load"]
-prompts = ["Here's my limerick mixed with a haiku: ", "It was the best of times, ", "omg lol like idk"]
+model_types = ["hf_load", "static_load", "irm_load", "irm_deactivated"]
+prompts = ["The 19th state to join the United States was ", "It was the best of times, ", "omg lol like idk", "Ok here's my joke: "]
 
 for model_type in model_types:
     print(f"Presenting outputs for {model_type}")
