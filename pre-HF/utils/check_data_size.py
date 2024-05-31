2f885ccad8492a67b8b11f9f8ff9762495254543
Dallin Baker <dfbaker5@login01.rc.byu.edu>
2024-05-31 14:00:05 -0600

    delete contents of pre-HF

--- pre-HF/utils/check_data_size.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load CSV into pandas dataframe
-df = pd.read_csv('/home/jo288/fsl_groups/grp_rocket/Rocket/dataset/raw/openorca_combined.csv')
-
-# Count number of rows
-num_rows = len(df.index)
-print(num_rows)
--- pre-HF/utils/train_test_split.py
+++
@@ -1,2 +0,0 @@
-# Take full tokenized data and do train/text split on it
-
