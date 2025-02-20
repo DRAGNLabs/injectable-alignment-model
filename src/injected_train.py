a0626690f2ab5bcf5bd4b42cd630a522fb852597
logicalpencils <82343982+logicalpencils@users.noreply.github.com>
2025-02-20 16:43:56 -0700

    Update hijacked_train.py

--- src/injected_train.py
+++ src/injected_train.py
@@ -13,7 +13,8 @@ from transformers import AutoTokenizer
 
 from lightning.dataset import DataModule
 from sp_tokenizer.tokenizer import Tokenizer as SPTokenizer
-from llama_models.injected_llama_for_causal import LlamaForCausalLM as Model
+from llama_models.llama_for_causal import LlamaForCausalLM as Model
+from llama_models.irm import IRM
 from utils.data_utils import Struct
 from tokenize_data import tokenize_data
 
@@ -64,6 +65,27 @@ def train(config):
     original_checkpoint_path = config.checkpoint_path
     print(f"Instantiating model")
     model = Model(tokenizer, config)
+
+    #setup injection
+    irm = IRM(config)
+    def hijack_attn(forward):
+        def injected_forward(*input,**keywords):
+            output = forward(*input,**keywords)
+            irm.forward(output[0])
+            return output
+        return injected_forward
+    
+    def hijack_layer(forward,irm,layer):
+        def injected_forward(*input,**keywords):
+            output = forward(*input,**keywords)
+            irm_out = irm.get_layer_weights(layer)
+            output = list(output)
+            output[0] += irm_out
+            return tuple(output)
+        return injected_forward
+    
+    model.model.layers[0].self_attn.forward = hijack_attn(model.model.layers[0].self_attn.forward)
+    for layer in irm.injection_layers: model.model.layers[layer].forward = hijack_layer(model.model.layers[layer].forward, irm, layer)
     
     # Load the model from the original checkpoint with strict=False, so it will only fill in the weights that are in both models, without errors
     print(f"Loading from checkpoint")
@@ -75,12 +97,16 @@ def train(config):
 
     # model.to("cuda" if "CUDA_VISIBLE_DEVICES" in os.environ else "cpu")
 
-    # Set requires_grad to false for everything
-    for param in model.parameters():
+    for i,param in enumerate(model.parameters()): 
+        #if i!=0: param.requires_grad = False
         param.requires_grad = False
+
+    # Set requires_grad to false for everything
+    #for param in model.parameters():
+    #    param.requires_grad = False
     # Except the IRM parameters
-    for irm_param in model.model.irm.parameters():
-        irm_param.requires_grad = True
+    #for irm_param in model.model.irm.parameters():
+    #    irm_param.requires_grad = True
 
     dm = DataModule(config, tokenizer)
 
